94ff3abc21b903c4987dfb0a5e38da3dcbfaf2f3
fcvirtual <austin@fcvt.dev>
2024-02-08 12:32:04 -0500

    feat: Refactor

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,14 +9,17 @@ contract FundMetest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
     }
+
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
+
     function testOwnerIsMsgSender() public {
         assertEq(fundMe.i_owner(), address(this));
     }
+
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);

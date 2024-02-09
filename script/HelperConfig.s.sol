9c5023e0d92f8ab96adaac4d1d72106714ffd457
fcvirtual <austin@fcvt.dev>
2024-02-09 11:35:02 -0500

    fix: HelperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,40 +5,51 @@ import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
-    NetworkConfig public activenetworkConfig;
+    NetworkConfig public activeNetworkConfig;
 
-    uint8 public constant DECIMAL = 8;
+    uint8 public constant DECIMALS = 8;
     int256 public constant INITIAL_PRICE = 2000e8;
+
     struct NetworkConfig {
         address priceFeed;
     }
 
+    event HelperConfig__CreatedMockPriceFeed(address priceFeed);
+
     constructor() {
-        if (block.chainid == uint256(11155111)) {
-            activenetworkConfig = getSepoliaEthConfig();
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
         } else {
-            activenetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory sepoliaConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    function getSepoliaEthConfig()
+        public
+        pure
+        returns (NetworkConfig memory sepoliaNetworkConfig)
+    {
+        sepoliaNetworkConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
         });
-        return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig()
+        public
+        returns (NetworkConfig memory anvilNetworkConfig)
+    {
+        // Check to see if we set an active network config
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
         vm.startBroadcast();
         MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
-            DECIMAL,
+            DECIMALS,
             INITIAL_PRICE
         );
         vm.stopBroadcast();
+        emit HelperConfig__CreatedMockPriceFeed(address(mockPriceFeed));
 
-        NetworkConfig memory anvilConfig = NetworkConfig({
-            priceFeed: address(mockPriceFeed)
-        });
-        return anvilConfig;
+        anvilNetworkConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
     }
 }
